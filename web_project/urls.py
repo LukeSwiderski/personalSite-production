89e2bfa2688216ea8566a3c059c009de7c153069
Luke Swiderski <Luke.Swiderski@gmail.com>
2021-04-23 11:26:48 -0400

    add import settings

--- web_project/urls.py
+++ web_project/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
 
 urlpatterns = [
     path("", include("personalSite.urls")),
