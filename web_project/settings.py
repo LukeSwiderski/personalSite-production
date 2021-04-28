cc6a0bbeb612eb2c6e82f91214476e3ffbef8ad9
Luke Swiderski <Luke.Swiderski@gmail.com>
2021-04-28 08:56:52 -0400

    add dot to lukeswiderski.com in allowed host

--- web_project/settings.py
+++ web_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["personal-site-luke.herokuapp.com", "lukeswiderski.com"]
+ALLOWED_HOSTS = ["personal-site-luke.herokuapp.com", ".lukeswiderski.com"]
 
 
 # Application definition
