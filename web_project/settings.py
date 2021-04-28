d6cce44baf148476b1338e68390563cafa2cad84
Luke Swiderski <Luke.Swiderski@gmail.com>
2021-04-28 08:53:24 -0400

    update allowed host

--- web_project/settings.py
+++ web_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["personal-site-luke.herokuapp.com", "127.0.0.1"]
+ALLOWED_HOSTS = ["personal-site-luke.herokuapp.com", "lukeswiderski.com"]
 
 
 # Application definition
